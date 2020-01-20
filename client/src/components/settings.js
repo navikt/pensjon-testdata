d32a23dc4dc4a567dec880a7c498bcf1f9a8dfe4
Erik Mohn <Erik.Mohn@nav.no>
2020-01-20 14:13:38 +0100

    Removed unused imports

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import {makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from "@material-ui/core/FormControlLabel";
--- client/src/components/sletttestdata.js
+++ client/src/components/sletttestdata.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Fareknapp} from "nav-frontend-knapper";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
