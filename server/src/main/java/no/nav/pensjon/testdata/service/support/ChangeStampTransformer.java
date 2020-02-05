8c4268573e408b98f9363b3430b1b2f68021461c
Erik Mohn <Erik.Mohn@nav.no>
2020-02-05 13:29:40 +0100

    refactor

--- server/src/main/java/no/nav/pensjon/testdata/service/support/ChangeStampTransformer.java
+++ server/src/main/java/no/nav/pensjon/testdata/service/support/ChangeStampTransformer.java
@@ -4,6 +4,6 @@ public class ChangeStampTransformer {
     public static String execute(String sql) {
         return sql
                 .replace("\"OPPRETTET_AV\"\\s=\\s'(.*?)'", "\"OPPRETTET_AV\" = 'PEN-SYNT-TD'")
-                .replace("\"ENDRET_AV\"\\s=\\s'(.*?)'", "\"ENDRET_AV\" = 'PEN-SYNT-TD-12837637812'");
+                .replace("\"ENDRET_AV\"\\s=\\s'(.*?)'", "\"ENDRET_AV\" = 'PEN-SYNT-TD'");
     }
 }
