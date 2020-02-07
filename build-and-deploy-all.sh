cd3b1d856ccd2871eec12ce2319a6b044f5b60d3
Erik Mohn <Erik.Mohn@nav.no>
2020-02-07 08:09:58 +0100

    Mindre endringer i byggscript

--- build-and-deploy-all.sh
+++ build-and-deploy-all.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
+echo "Angi versjon: "
+read version
 
 cd ./server/
-cat VERSION | awk '{print $1+1}' > VERSION
-serverVersion=`cat VERSION`
-imagenameServer="repo.adeo.no:5443/navikt/pensjon-testdata-server:$serverVersion"
-echo "Bumped $imagenameServer version to: $serverVersion"
+imagenameServer="repo.adeo.no:5443/navikt/pensjon-testdata-server:$version"
+
 docker build -t $imagenameServer .
 docker push $imagenameServer
 
 cd ../client/
-cat VERSION | awk '{print $1+1}' > VERSION
-clientVersion=`cat VERSION`
-imagenameClient="repo.adeo.no:5443/navikt/pensjon-testdata-client:$clientVersion"
-echo "Bumped $imagenameClient version to: $serverVersion"
+imagenameClient="repo.adeo.no:5443/navikt/pensjon-testdata-client:$version"
 docker build -t $imagenameClient .
 docker push $imagenameClient
 
 cd ..
 kubectl config use-context dev-fss
-
-sed "s/IMAGE_VERSION/$serverVersion/g" server/nais.yaml | kubectl apply -f-
-sed "s/IMAGE_VERSION/$clientVersion/g" client/nais.yaml | kubectl apply -f-
+sed "s/IMAGE_VERSION/$version/g" server/nais.yaml | kubectl apply -f-
+sed "s/IMAGE_VERSION/$version/g" client/nais.yaml | kubectl apply -f-
