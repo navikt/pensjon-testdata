1a3f3a240b3cd200133f2f0e449b45161b9e9d77
Helge Janbu <helge.janbu@nav.no>
2020-03-03 16:28:31 +0100

    Inntekt get/set

--- dolly-facade/src/main/java/no/nav/pensjon/testdata/controller/support/response/Inntekt.java
+++ dolly-facade/src/main/java/no/nav/pensjon/testdata/controller/support/response/Inntekt.java
@@ -5,4 +5,36 @@ public class Inntekt {
     private String fnr;
     private int InntektAar;
     private long belop;
+
+    public String getMiljo() {
+        return miljo;
+    }
+
+    public void setMiljo(String miljo) {
+        this.miljo = miljo;
+    }
+
+    public String getFnr() {
+        return fnr;
+    }
+
+    public void setFnr(String fnr) {
+        this.fnr = fnr;
+    }
+
+    public int getInntektAar() {
+        return InntektAar;
+    }
+
+    public void setInntektAar(int inntektAar) {
+        InntektAar = inntektAar;
+    }
+
+    public long getBelop() {
+        return belop;
+    }
+
+    public void setBelop(long belop) {
+        this.belop = belop;
+    }
 }
