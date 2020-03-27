4d8629ffb62c6aab611c6ea32aa1d657c7581dca
Anton Fofanov <anton.fofanov@nav.no>
2020-03-27 17:21:40 +0100

    utvide returnerte scenarios fra serveren

--- server/src/main/java/no/nav/pensjon/testdata/repository/ScenarioRepository.java
+++ server/src/main/java/no/nav/pensjon/testdata/repository/ScenarioRepository.java
@@ -35,7 +35,7 @@ public class ScenarioRepository {
                 }
             }
         }
-        PrimaryKeySwapper.initializePrimaryKeyRegistry(testScenario.getAllePersoner());
+        PrimaryKeySwapper.initializePrimaryKeyRegistry(testScenario.getAllePersonIds());
         return testScenario;
     }
 
