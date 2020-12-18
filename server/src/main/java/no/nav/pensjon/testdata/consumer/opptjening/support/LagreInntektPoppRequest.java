7b73c94ebaf627bbeccead81e3a0c7a79148c405
Anton Fofanov <anton.fofanov@nav.no>
2020-12-18 14:03:17 +0100

    legg til toString til lagreinntektObjektet

--- server/src/main/java/no/nav/pensjon/testdata/consumer/opptjening/support/LagreInntektPoppRequest.java
+++ server/src/main/java/no/nav/pensjon/testdata/consumer/opptjening/support/LagreInntektPoppRequest.java
@@ -2,11 +2,20 @@ package no.nav.pensjon.testdata.consumer.opptjening.support;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.StringJoiner;
+
 public class LagreInntektPoppRequest {
     @JsonProperty
     private Inntekt inntekt;
 
     public LagreInntektPoppRequest(Inntekt inntekt) {
         this.inntekt = inntekt;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", LagreInntektPoppRequest.class.getSimpleName() + "[", "]")
+                .add("inntekt=" + inntekt)
+                .toString();
     }
 }
